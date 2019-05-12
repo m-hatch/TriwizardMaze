444ecfd800ac98688c28da30b4a98806165bf0bc
Montgomery Hatch <montehatch@gmail.com>
2019-05-12 16:36:47 -0400

    Add Controls

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Map from './js/Map'
+import Controls from './js/Controls'
 import GameLoop from './js/GameLoop'
 import sampleMaze from './mazes/sampleMaze'
 import './styles/main.scss'
 
 const map = new Map(mazeGrid.size)
+const controls = new Controls();
 const loop = new GameLoop();
 
 map.load(sampleMaze)
