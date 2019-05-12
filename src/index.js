0b02d415e8357a7a61a1abf2757a4c97ff89aea8
Montgomery Hatch <montehatch@gmail.com>
2019-05-12 16:47:13 -0400

    Add Player

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import Map from './js/Map'
+import Player from './js/Player'
 import Controls from './js/Controls'
 import GameLoop from './js/GameLoop'
 import sampleMaze from './mazes/sampleMaze'
 import './styles/main.scss'
 
-const map = new Map(mazeGrid.size)
+const map = new Map(sampleMaze.size)
+const player = new Player(4.5, 2.5, Math.PI * 0.5)
 const controls = new Controls();
 const loop = new GameLoop();
 
 map.load(sampleMaze)
 
+// Run the game loop
 loop.start(function frame(seconds) {
   map.update(seconds)
+  player.update(controls.states, map, seconds)
 })
---
+++ src/util/constants.js
@@ -0,0 +1,7 @@
+export const CIRCLE = Math.PI * 2
+export const MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
+
+export default {
+  CIRCLE,
+  MOBILE
+}
