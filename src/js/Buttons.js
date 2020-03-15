da24e95ea1ca09b52162298de336216f2f3470d8
Montgomery Hatch <montehatch@gmail.com>
2020-03-15 19:57:29 -0400

    Add active styles to buttons

--- src/js/Buttons.js
+++ src/js/Buttons.js
@@ -9,15 +9,17 @@ class Buttons {
     if (nav) {
       this.navigation = nav
       const mapIcon = this.createIcon(['fa', 'fa-globe-americas'])
-      const navBtn = this.createButton('btn-nav', mapIcon)
+      const navBtn = this.createButton('btn-nav', mapIcon, true)
       document.getElementsByClassName('buttons')[0].appendChild(navBtn)
     }
   }
 
-  createButton(id, icon) {
+  createButton(id, icon, active) {
     const elem = document.createElement('button')
+    const classes = active ? ['buttons__btn', 'buttons__btn--active'] : ['buttons__btn']
+
     elem.setAttribute('id', 'btn-nav')
-    elem.classList.add('buttons__btn')
+    elem.classList.add(...classes)
     elem.appendChild(icon)
     elem.onclick = this.handleClick
     return elem
@@ -30,14 +32,16 @@ class Buttons {
     return elem
   }
 
-  toggle(elem) {
+  toggle(elem, btn) {
     elem.classList.toggle('hide')
+    btn.classList.toggle('buttons__btn--active')
   }
 
   handleClick(e) {
     // We check the propagation chain for our target elem
-    if (e.path.find(target => target.id === 'btn-nav')) {
-      this.toggle(this.navigation.target)
+    const navBtn = e.path.find(target => target.id === 'btn-nav')
+    if (navBtn) {
+      this.toggle(this.navigation.target, navBtn)
     }
   }
 }
