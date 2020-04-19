41b45aec6670c93b0ef43de063e3be02e47f2dba
Montgomery Hatch <montehatch@gmail.com>
2020-04-19 18:57:57 -0400

    Fix paths for bundle

--- src/js/Map.js
+++ src/js/Map.js
@@ -4,8 +4,8 @@ class Map {
   constructor (size) {
     this.size = size
     this.wallGrid = new Uint8Array(size * size)
-    this.skybox = new Bitmap('../images/deathvalley_panorama.jpg', 2000, 750)
-    this.wallTexture = new Bitmap('../images/hedge.jpg', 1600, 1600);
+    this.skybox = new Bitmap('images/deathvalley_panorama.jpg', 2000, 750)
+    this.wallTexture = new Bitmap('images/hedge.jpg', 1600, 1600);
     this.light = 2
   }
 
